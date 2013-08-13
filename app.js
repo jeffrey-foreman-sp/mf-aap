9fcfdbf91f04c7cc1edc975880690e290126f990
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-08-13 16:29:49 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+Ext.application({
+    name: 'Aap',
+
+    appFolder: 'app',
+
+    launch: function() {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    xtype: 'panel',
+                    title: 'Users',
+                    html : 'List of users will go here'
+                }
+            ]
+        });
+    }
+});
