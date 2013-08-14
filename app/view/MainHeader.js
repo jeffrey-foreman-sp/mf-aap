f0101cc895995dfec63ee67d173d8f6c9ddf5c4d
rogerbaer <rogerbaer@roger-bars-macbook-pro.local>
2013-08-15 00:09:24 +0200

    make header layout

--- app/view/MainHeader.js
+++ app/view/MainHeader.js
@@ -1,16 +1,45 @@
 Ext.define('Aap.view.MainHeader',{
 	extend: 'Ext.panel.Panel',
-	xtype: 'mainheader'/*,
+	xtype: 'mainheader',
+	buttonAlign: 'center',
+	layout: {
+	        type: 'hbox',
+	},
+	defaults: {
+//		border: 0
+	},
 	items: [
 		{
-			xtype: 'button',
-			text: 'Bearbeiten'
+			xtype: 'panel',
+			flex: 4
 		},{
-			xtype: 'button',
-			text: 'Anmelden'
+			xtype: 'panel',
+			flex: 1,
+			items: [
+				{
+					xtype: 'button',
+					text: 'Anmelden'
+				}
+			]
 		},{
-			xtype: 'button',
-			text: 'Abmelden'
+			xtype: 'panel',
+			flex: 1,
+			items: [
+				{
+					xtype: 'button',
+					text: 'Bearbeiten'
+				}
+			]
+		},{
+			xtype: 'panel',
+			flex: 1,
+			items: [
+				{
+					xtype: 'button',
+					text: 'Informationen'
+				}
+			]
 		}
-	]*/
+	]
+
 });
