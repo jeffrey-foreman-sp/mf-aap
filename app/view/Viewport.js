db8963f02dd5a56c81d46a16324d4feaae8ec97e
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-08-14 12:19:52 +0200

    build main scaffold of layout

--- app/view/Viewport.js
+++ app/view/Viewport.js
@@ -1,33 +1,72 @@
 Ext.define('HR.view.Viewport', {
 	extend: 'Ext.container.Viewport',
-	requires: ['HR.view.company.List'],
+//	requires: ['HR.view.company.List'],
+	
 	layout: {
 		type:  'vbox',
-		align: 'stretch'
+		align: 'center',
+		pack: 'center'
 	},
+	autoScroll: true,
 	items:[
-		{	xtype: 'toolbar',	
+		{
+			xtype: 'panel',
+			height: 800,
+			width: 1200,
+			style: 'margin-top: auto; margin-bottom: auto;',	
+			layout: {
+				align: 'stretch'
+			},
 			items: [
-				 {	xtype: 'button',
-					text: 'Fill',
-					action: 'load'
-				},
-				{	xtype: 'button',
-					text: 'Commit',
-					action: 'save',
-					disabled: true
+				{
+					xtype: 'panel',
+					height: 60
+				},{
+					xtype: 'panel',
+					height: 740,	
+					layout: {
+						type: 'hbox',
+						align: 'stretch'
+					},
+					items: [
+						{
+							xtype: 'panel',
+							title: 'Geodatenbestand',
+							flex: 1
+						},{
+							xtype: 'panel',
+							title: 'Metadaten Allgemein',
+							flex: 1,
+							items: [
+								{
+									xtype: 'panel',
+									title: 'Allgemein',
+									flex: 1
+								}
+							]
+						},{
+							xtype: 'panel',
+							title: 'Metadaten AAP',
+							flex: 2, 
+							layout: {
+								type: 'hbox',
+								align: 'stretch'
+							},
+							items: [
+								{
+									xtype: 'panel',
+									title: 'Bewertung Nachhaltige Verfügbarkeit',
+									flex: 1
+								},{
+									xtype: 'panel',
+									title: 'Bewertung Archivwürdigkeit',
+									flex: 1
+								}	
+							]	
+						}
+					]
 				}
 			]
-		},
-		{	xtype: 'companylist',
-			title: 'Company List',
-			flex: 1
-		},
-		{	xtype: 'splitter'
-		}, 	
-		{	xtype: 'panel',
-			title: 'Associate List',
-			flex: 1
 		}
 	]
 });
