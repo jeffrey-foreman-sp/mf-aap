209205264deeb6b0b62ba3f040d18e506fbc643d
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-08-19 10:49:30 +0200

    delet needless files

--- app/model/Company.js
+++
@@ -1,7 +0,0 @@
-Ext.define('HR.model.Company', {
-	extend: 'Ext.data.Model',
-
-	fields: [
-		{name: 'name', type: 'string'}
-	]
-});
--- app/store/Companies.js
+++
@@ -1,8 +0,0 @@
-Ext.define('HR.store.Companies', {
-	extend: 'Ext.data.Store',
-	model: 'HR.model.Company',
-	data: [
-		{id: 1, name: 'Froata Systems'},
-		{id: 2, name: 'Sencha'}
-	]
-});
