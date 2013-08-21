087744e8be9a7bf35a40e1d3ba50517a6127025c
rogerbaer <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-08-21 09:40:28 +0200

    change tree controller

--- app/controller/Tree.js
+++ app/controller/Tree.js
@@ -21,18 +21,16 @@ Ext.define('Aap.controller.Tree', {
 
 	addNode: function() {
 
-/*		var treeelement = Ext.getCmp('treestructure');
-		var selectionmodel = treeelement.getSelectionModel();
-		var selection = selectionmodel.getSelection()[0];
-		var nodeid = selection.getId();	
-*/
-
+	//var newNode = Ext.create("Aap.model.TreeNode", {name: "TestOffice"});
+		
 		var nodeid = Aap.util.SelectedNode.getIdFromSelectedNode();				
 		var store = Ext.getStore('TreeStore');
-		var newNode = Ext.create("Aap.model.TreeNode", {name: "TestOffice"});
-		
+	
 		var selectednode = store.getNodeById(nodeid); 
-		selectednode.appendChild("newNode");
+		selectednode.appendChild({
+			name: 'newNode',
+			leaf: true
+		});
 		selectednode.expand();
 
 
