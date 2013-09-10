98b4d30ed8b28c7ccb031171e608201aebe9d9cc
rogerbaer <roger.baer@swisstopo.ch>
2013-09-10 09:33:01 +0200

    adjust fields of allgemein model

--- app/model/Allgemein.js
+++ app/model/Allgemein.js
@@ -1,25 +1,24 @@
 Ext.define('Aap.model.Allgemein', {
  	extend: 'Ext.data.Model',
 	fields: [
+        {name: "id", type: 'int'}, 
         {name: "name", type: 'string'}, 
-		{name: "ident", type: 'string'}, 
-		{name: "georefdat", type: 'string'},
+		{name: "ident", type: 'float'}, 
+		{name: "georefdat", type: 'boolean'},
 		{name: "fachst", type: 'string'}, 
 		{name: "zugberech", type: 'string'}, 
 		{name: "echkateg", type: 'string'}, 
 		{name: "nachfzeitr", type: 'string'}, 
-		{name: "datenmenge"}, 
-		{name: "imjr"}, 
-		{name: "datenzuw"}, 
-		{name: "bemerk", type: 'string'},
-		{name: "treenode_id", type: 'string'}
+		{name: "datenmenge", type: 'int'}, 
+		{name: "imjr", type: 'float'}, 
+		{name: "datenzuw", type: 'float'}, 
+		{name: "bemerk", type: 'string'}
 	],
 	belongsTo: 'Aap.model.TreeNode',
 	proxy: {
 		type: 'ajax',
 		api: {
-			read: 'data/allgemeindata.json',
-			update: 'data/update.json'
+			read: 'data/allgemeindata.json'
 		},	
 		reader: {
 			type: 'json',
