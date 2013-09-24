c88852ea78d1923537b8974346212367f38afe30
rogerbaer <rogerbaer@roger-bars-macbook-pro.local>
2013-09-24 22:58:12 +0200

    modify display layout

--- app/view/mainbody/Allgemein.js
+++ app/view/mainbody/Allgemein.js
@@ -4,19 +4,24 @@ Ext.define('Aap.view.mainbody.Allgemein',{
 	xtype: 'allgemein',
 	alias: 'widget.displayallgemein',
 	id: 'disp_allg',
-	bodyPadding: 10,
+	
 	tpl: [
-		'<p>Name: <strong>{name}</strong></p>',
-		'<p>Identifikator (Sammlung): <strong>{ident}</strong></p>',
-		'<p>Georeferenzdaten: <strong>{georefdat}</strong></p>',
-		'<p>Fachstelle des Bundes: <strong>{fachst}</strong></p>',
-		'<p>Zugangsberechtigungsstufe: <strong>{zugberech}</strong></p>',
-		'<p>eCH-Kategorie: <strong>{echkateg}</strong></p>',
-		'<p>Nachführungsrythmus: <strong>{nachfrhythm}</strong></p>',
-		'<p>Nachführungszeitraum: <strong>{nachfzeitraum}</strong></p>',
-		'<p>Datenmenge (in GB): <strong>{datenmenge}</strong></p>',
-		'<p>im Jahr: <strong>{imjr}</strong></p>',
-		'<p>Datenzuwachs (GB pro Jahr): <strong>{datenzuw}</strong></p>',
-		'<p>Bemerkung: <strong>{bemerk}</strong></p>'
+		'<table style="font-size: 12px;">',
+		'<tr><td>Name: </td><td>{name}</td></tr>',
+		'</table>',
+		'<table style="font-size: 12px;">',
+		'<tr><td>Name: </td><td>{name}</td></tr>',
+		'<tr><td>Identifikator (Sammlung): </td><td>{ident}</td></tr>',
+		'<tr><td>Georeferenzdaten: </td><td>{georefdat}</td></tr>',
+		'<tr><td>Fachstelle des Bundes: </td><td>{fachst}</td></tr>',
+		'<tr><td>Zugangsberechtigungsstufe: </td><td>{zugberech}</td></tr>',
+		'<tr><td>eCH-Kategorie: </td><td>{echkateg}</td></tr>',
+		'<tr><td>Nachführungsrythmus: </td><td>{nachfrhythm}</td></tr>',
+		'<tr><td>Nachführungszeitraum: </td><td>{nachfzeitraum}</td></tr>',
+		'<tr><td>Datenmenge (in GB): </td><td>{datenmenge}</td></tr>',
+		'<tr><td>im Jahr: </td><td>{imjr}</td></tr>',
+		'<tr><td>Datenzuwachs (GB pro Jahr): </td><td>{datenzuw}</td></tr>',
+		'<tr><td>Bemerkung: </td><td>{bemerk}</td></tr>',
+		'</table>'
 	]	
 });
