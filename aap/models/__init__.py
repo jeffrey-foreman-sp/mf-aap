a8e40e02ae480915d3a3de3b2e30ffb7048982e0
Marc Monnerat <procrastinatio@gmail.com>
2013-11-04 16:49:20 +0100

    adding locking

--- aap/models/__init__.py
+++ aap/models/__init__.py
@@ -1,5 +1,9 @@
 import os
 import datetime
+import time
+import uuid
+import random
+import json
 
 from boto import config
 from boto.s3.connection import S3Connection
@@ -16,6 +20,7 @@ from sqlalchemy import (
     Integer,
     Text,
     DateTime,
+    exc,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -23,6 +28,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
+    object_session,
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
@@ -62,6 +68,50 @@ class Lock(Base):
         self.id = id
         self.attributes = attributes
 
+   
+    @classmethod
+    def acquire_lock(class_, id, lock_duration, acquire_timeout_seconds):
+        lockId = uuid.uuid4().hex
+        acquire_timeout = time.time() + acquire_timeout_seconds
+        while time.time() < acquire_timeout:
+                lock = DBSession.query(class_).get(id)
+                if lock:
+                    # check for staled lock
+                    attribs = json.loads(lock.attributes)
+                    if attribs.has_key('timeout') and float(attribs['timeout']) < time.time():
+                        # lock has timed out --> delete
+                        print "stalled"
+                        class_.release_lock(id, attribs['lockId'])  
+                else:
+                    # try create one
+                    lock_timeout = time.time() + lock_duration
+                    Lock = class_
+                    lock = Lock(id, json.dumps({ 'timeout' : lock_timeout, 'lockId' : lockId }))
+                    DBSession.add(lock)
+                    DBSession.flush()
+                
+                    return lockId
+
+                time.sleep(0.05)
+        # 
+        return False
+    
+    @classmethod
+    def release_lock(class_, id, lockId):
+        try:
+            lock = DBSession.query(class_).get(id)
+            if lock:
+                DBSession.delete(lock)
+                DBSession.flush()
+
+                return True
+            else:
+                return False
+        except exc.SQLAlchemyError,e:
+            raise e
+   
+    
+
 class User(Base):
     """ The SQLAlchemy declarative model class for a User object. """
     __tablename__ = 'users'
@@ -73,6 +123,10 @@ class User(Base):
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        
+    @classmethod
+    def get(class_, id):
+        return DBSession.query(class_).get(id)
 
     @classmethod
     def is_known(cls, username):
